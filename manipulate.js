35040ad25dce5d610788db4158e772787d29ddb3
Thachin <nickethachin@gmail.com>
2024-01-04 17:01:19 +0700

    chore(permutation): Formatted file and add instruction

--- manipulate.js
+++ manipulate.js
@@ -1,12 +1,15 @@
 /**
- * 
+ *
  * @param {string} text string of value example "aabb", "abcde"
  * @returns {string[]} string array of result
  */
 const manipulate = (text) => {
-    // TODO : start your code here
-}
+  /* ------------------------------- INSTRUCTION ------------------------------ */
+  // Create all permutations of a non-empty input string
+  /* -------------------------------------------------------------------------- */
+  // TODO : start your code here
+};
 
 module.exports = {
-    manipulate
-}
+  manipulate,
+};
