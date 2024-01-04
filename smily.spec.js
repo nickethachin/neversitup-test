52d4e8a3069d21e027ae9b2d3c29b8413a25752d
Thachin <nickethachin@gmail.com>
2024-01-04 17:20:34 +0700

    test(smilycount): Add test as instructed

--- smily.spec.js
+++ smily.spec.js
@@ -1,1 +1,24 @@
 // Write your test here
+const { countSmilyFace } = require("./smily");
+
+describe("countSmilyFace function", () => {
+  it("should return 0 for an empty array", () => {
+    const result = countSmilyFace([]);
+    expect(result).toEqual(0);
+  });
+
+  it("should return 0 for an empty array", () => {
+    const result = countSmilyFace([":)", ";(", ";}", ":-D"]);
+    expect(result).toEqual(2);
+  });
+
+  it("should return 0 for an empty array", () => {
+    const result = countSmilyFace([";D", ":-(", ":-)", ";~)"]);
+    expect(result).toEqual(3);
+  });
+
+  it("should return 0 for an empty array", () => {
+    const result = countSmilyFace([";]", ":[", ";*", ":$", ";-D"]);
+    expect(result).toEqual(1);
+  });
+});
