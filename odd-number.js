95c4e79b83d31dbe0fa977772465a2ec587c28fc
Thachin <nickethachin@gmail.com>
2024-01-04 17:07:25 +0700

    chore(findoddint): Formatted file and add instruction

--- odd-number.js
+++ odd-number.js
@@ -1,13 +1,16 @@
 /**
- * 
+ *
  * @param {number[]} numbers list of numbers example [7], [0], [1,1,2], [0,1,0,0,1], [1,2,3,4,5,6,7,8,9,0]
  * @returns {number} one number that odd number
  */
 const findOddNumber = (text) => {
-    // TODO : start your code here
-}
-
+  /* ------------------------------- INSTRUCTION ------------------------------ */
+  // Given an array of integers, find the one that appears an odd number of times.
+  // There will always be only one integer that appears an odd number of times.
+  /* -------------------------------------------------------------------------- */
+  // TODO : start your code here
+};
 
 module.exports = {
-    findOddNumber
-}
+  findOddNumber,
+};
