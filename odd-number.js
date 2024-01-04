d5fa9ef690091cec1b65c0ad545594a28945f82f
Thachin <nickethachin@gmail.com>
2024-01-04 17:07:58 +0700

    feat(findoddint): Add functionality

--- odd-number.js
+++ odd-number.js
@@ -9,6 +9,23 @@ const findOddNumber = (text) => {
   // There will always be only one integer that appears an odd number of times.
   /* -------------------------------------------------------------------------- */
   // TODO : start your code here
+
+  if (text.length === 0) return 0;
+
+  const result = [];
+  const count = {};
+
+  text.forEach((number) => {
+    count[number] = (count[number] || 0) + 1;
+  });
+
+  for (let key in count) {
+    if (count[key] % 2 !== 0) {
+      result.push(key);
+    }
+  }
+
+  return Number(result[0]);
 };
 
 module.exports = {
