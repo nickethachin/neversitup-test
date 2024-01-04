b4934d7ce8e46af9cec92ed2b06d732982344879
Thachin <nickethachin@gmail.com>
2024-01-04 17:02:46 +0700

    feat(permutation): Add functionality

--- manipulate.js
+++ manipulate.js
@@ -8,6 +8,23 @@ const manipulate = (text) => {
   // Create all permutations of a non-empty input string
   /* -------------------------------------------------------------------------- */
   // TODO : start your code here
+
+  if (text.length === 0) return [];
+  const result = [];
+
+  const permute = (_string, current = "") => {
+    if (_string.length === 0) result.push(current);
+    _string.split("").forEach((char, index) => {
+      const nextChars = _string.slice(0, index) + _string.slice(index + 1);
+      permute(nextChars, current + char);
+    });
+  };
+
+  permute(text);
+
+  const unique = [...new Set(result)];
+  
+  return unique;
 };
 
 module.exports = {
